661695525a63d117aebb0a75da91bf974afe6ccc
Marie Goff <mariegoff46@gmail.com>
2024-10-25 12:27:40 -0400

    Fix year_folder

--- fileOrganizer/fileOrganizer.py
+++ fileOrganizer/fileOrganizer.py
@@ -37,9 +37,6 @@ def create_folder(file_path, source_folder):
 if __name__ == "__main__":
     # Specify folder to organize
     source_folder = r"C:/Users/marie/OneDrive/Documents/PythonTest/"
-    try:
-        print(os.listdir(source_folder))
-    except FileNotFoundError as e:
-        print(f"Error: {e}")
-    #organize_files_by_year(source_folder)
+    organize_files_by_year(source_folder)
+
 
